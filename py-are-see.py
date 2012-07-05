529ac67a8312f8601a137ad729f3d71498341db1
Anthony Van de Gejuchte <anthonyvdgent@yahoo.com>
2012-07-06 00:43:54 +0200

    Move main code into a (yet) useless class
    There are still lots of things to decide and to try out. The most important
    one now is picking the right libraries without bloating dependencies.
    Expect a lot of full code rewrites and changing plans.

--- py-are-see.py
+++ py-are-see.py
@@ -5,23 +5,28 @@ from datetime import datetime
 
 from src.settings import settings
 
-s = socket.socket()
-s.connect((settings["host"], settings["port"]))
-s.send("NICK %s\r\n" % settings["name"])
-s.send("USER %s %s bla :%s\r\n" % (settings["ident"], settings["host"], settings["realname"]))
-s.send("JOIN :%s\r\n" % settings["chan"])
+class bot:
+    def __init__(self):
+        s = socket.socket()
+        s.connect((settings["host"], settings["port"]))
+        s.send("NICK %s\r\n" % settings["name"])
+        s.send("USER %s %s bla :%s\r\n" % (settings["ident"], settings["host"], settings["realname"]))
+        s.send("JOIN :%s\r\n" % settings["chan"])
 
-readbuffer = ""
-while True:
-    readbuffer = readbuffer+s.recv(1024)
-    temp = readbuffer.split("\n")
-    readbuffer = temp.pop( )
+        readbuffer = ""
+        while True:
+            readbuffer = readbuffer+s.recv(1024)
+            temp = readbuffer.split("\n")
+            readbuffer = temp.pop( )
 
-    for line in temp:
-        line = line.rstrip().split()
+            for line in temp:
+                line = line.rstrip().split()
 
-        print datetime.now(), ' <- ', line
+                print datetime.now(), ' <- ', line
 
-        if (line[0] == "PING"):
-            print datetime.now(), ' -> PONG %s\r\n' % line[1]
-            s.send("PONG %s\r\n" % line[1])
+                if (line[0] == "PING"):
+                    print datetime.now(), ' -> PONG %s\r\n' % line[1]
+                    s.send("PONG %s\r\n" % line[1])
+
+if __name__ == "__main__":
+    bot()
